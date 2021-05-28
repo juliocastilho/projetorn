7cf1f244cbaf3d9e1d09010654cc1e162fa85750
Julio Castilho <juliocastillho@gmail.com>
2021-05-28 14:11:30 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import {AppRegistry} from 'react-native';
-import Navigator from './src/Navigator';
+//import Navigator from './src/Navigator';
+import Login from './src/';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Navigator);
+AppRegistry.registerComponent(appName, () => Login);
