584d758c0447c340d6c9c92ba205e14c8a03e7a8
Julio Castilho <juliocastillho@gmail.com>
2021-05-29 10:29:16 -0300

    Criando do zero LOGIN
    Sem animaçao

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import {AppRegistry} from 'react-native';
 //import Navigator from './src/navigator';
-import login from './src/views/login';
+import Login from './src/views/login';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => login);
+AppRegistry.registerComponent(appName, () => Login);
--- src/views/login.js
+++ src/views/login.js
@@ -1,149 +1,93 @@
-import React, { useState, useEffect} from 'react'
-import {
-KeyboardAvoidingView,
-TextInput,
-TouchableOpacity,
-Text,
-StyleSheet,
-Animated,
-Keyboard,
-SafeAreaView } from 'react-native'
+import React from 'react'
+import { KeyboardAvoidingView, 
+         SafeAreaView, 
+         Image, 
+         View, 
+         TextInput, 
+         TouchableOpacity,
+         Text,  
+         StyleSheet 
+} from 'react-native'
+
 
 export default function login(){
-
-    const [offset] = useState (new Animated.ValueXY({x:0, y:95}));
-    const [opacity] = useState(new Animated.Value(0));
-    const [logo] =    useState(new Animated.Value({x:130, y:155}));
-
-    useEffect(() => {
-    keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', keyboardDidShow);
-    keyboarDidHideListener  = Keyboard.addListener('keyboardDidHide', keyboardDidHide);
-
-       Animated.parallel([
-          Animated.spring(offset.y, {
-            toValue: 0,
-            speed: 4,
-            bounciness: 20
-          }),
-        
-          Animated.timing(opacity, {
-            toValue: 1,
-            duration: 200,
-          })
-        ]).start();
-    });
-    
-
-
-    function keyboardDidShow(){
-        Animated.parallel([
-            Animated.timing(logo.x, {
-                toValue: 55,
-                duration: 100,
-            }),
-            Animated.timing(logo.y, {
-                toValue: 65,
-                duration: 100,
-            }),
-        ])
-    }
-
-    function keyboardDidHide(){
-        Animated.parallel([
-            Animated.timing(logo.x, {
-                toValue: 130,
-                duration: 100,
-            }),
-            Animated.timing(logo.y, {
-                toValue: 155,
-                duration: 100,
-            }),
-        ]).start();
-    }
-
 
 
     return(
         <KeyboardAvoidingView style={styles.backgoud}>
-            <SafeAreaView style={styles.containerLogo}>
-                <Animated.Image
-                  style={{
-                      width: logo.x,
-                      height:logo.y,
-                    }}
-                    source= { require('../../assets/img/logo.png')}
-                />
-            </SafeAreaView>
-             
-            <Animated.View style={[styles.container, { opacity: opacity, transform:[{ translateY }]}]} >
+        <SafeAreaView style={styles.containerLogo}> 
+        <Image source = {require('../../assets/img/logo.png')} />
+       </SafeAreaView>
 
-
-                <TextInput
+       <View style={styles.container}>
+            <TextInput
                 style={styles.input}
                 placeholder="Email"
                 autoCorrect={false}
                 onChangeText={() => {}}
-                />
+            />
 
-                <TextInput
+            <TextInput
                 style={styles.input}
                 placeholder="Senha"
                 autoCorrect={false}
                 onChangeText={() => {}}
-                />
+            />
 
+            <TouchableOpacity style={styles.btnSubmit}>
+                    <Text style={styles.btnText}>Entrar</Text>
+            </TouchableOpacity>
 
-                <TouchableOpacity>
+            <TouchableOpacity >
                     <Text></Text>
-                </TouchableOpacity>
+            </TouchableOpacity>
 
-                <TouchableOpacity>
-                    <Text></Text>
-                </TouchableOpacity>
+       </View>
 
-            </Animated.View>
-        </KeyboardAvoidingView>
+       </KeyboardAvoidingView>
     )
 }
 
 
-
 const styles = StyleSheet.create({
 
-     backgoud:{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#191919'
-     },
+    backgoud:{
+       flex: 1,
+       alignItems: 'center',
+       justifyContent: 'center',
+       backgroundColor: '#191919'
+    },
 
-     containerLogo:{
-        flex: 1,
-        justifyContent: 'center'
-     },
-     container:{
-        flex:1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: '90%',
-     },
-     input:{
-        backgroundColor: '#FFF',
-        width: '90%',
-        marginBottom: 15,
-        color: '#222',
-        fontSize: 17,
-        borderRadius: 7,
-        padding: 10
-     },
-     btnSubmit:{
-        backgroundColor: '#35AAFF',
-        width: '90%',
-        height: '45',
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 7
-
-     }
+    containerLogo:{
+       flex: 1,
+       alignItems: 'center',
+       justifyContent: 'center',
+    },
+    container:{
+       flex:1,
+       alignItems: 'center',
+       width: '90%',
+    },
+    input:{
+       backgroundColor: '#FFF',
+       width: '90%',
+       marginBottom: 15,
+       color: '#222',
+       fontSize: 17,
+       borderRadius: 7,
+       padding: 10,
+    },
+    btnSubmit:{
+       backgroundColor: '#35AAFF',
+       width: '90%',
+       height: 45,
+       alignItems: 'center',
+       justifyContent: 'center',
+       borderRadius: 7
+    },
+    btnText:{
+       color: '#FFF',
+       fontSize: 16,
+    },
 
 })
